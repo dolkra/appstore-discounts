e1be736aa017d181d09b033b9eb5c15da2a88ff3
GitHub Workflow <eyelly.wu@gmail.com>
2025-08-13 23:36:26 +0000

    chore: automatic update of GitHub workflow

--- appinfo.config.ts
+++ appinfo.config.ts
@@ -68,6 +68,13 @@ export const regionTimezoneMap: Record<Region, string> = {
  */
 export const latestAppConfig: AppConfig[] = [
   {
+    id: 1064060287,
+    name: {
+      mo: 'XP3Player',
+    },
+    addType: 'auto',
+  },
+  {
     id: 6747717070,
     name: {
       mo: 'VoidLink',
