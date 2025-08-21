fb66277b9e33ed39a55cf6ff3c454ec30be272f7
GitHub Workflow <eyelly.wu@gmail.com>
2025-08-21 12:48:15 +0000

    chore: automatic update of GitHub workflow

--- appinfo.config.ts
+++ appinfo.config.ts
@@ -68,6 +68,13 @@ export const regionTimezoneMap: Record<Region, string> = {
  */
 export const latestAppConfig: AppConfig[] = [
   {
+    id: 982102012,
+    name: {
+      mo: '三姑六婆 Pro - 親戚稱呼計算機',
+    },
+    addType: 'auto',
+  },
+  {
     id: 6747814605,
     name: {
       mo: 'Is This Seat Taken?',
