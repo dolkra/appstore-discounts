e2fefd04b61bbcce61492dcd64a5a7516782c842
GitHub Workflow <eyelly.wu@gmail.com>
2025-10-02 14:38:14 +0000

    chore: automatic update of GitHub workflow

--- appinfo.config.ts
+++ appinfo.config.ts
@@ -68,6 +68,13 @@ export const regionTimezoneMap: Record<Region, string> = {
  */
 export const latestAppConfig: AppConfig[] = [
   {
+    id: 1588558691,
+    name: {
+      mo: 'Math Master | 數學遊戲 火柴拼圖 | 交叉数学',
+    },
+    addType: 'auto',
+  },
+  {
     id: 6738239220,
     name: {
       mo: '电工证考试题库-2024最新电工考试题库',
