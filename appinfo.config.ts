8ed74e4229a04dc2181a07511b9a63e464fab475
GitHub Workflow <eyelly.wu@gmail.com>
2025-10-08 12:51:16 +0000

    chore: automatic update of GitHub workflow

--- appinfo.config.ts
+++ appinfo.config.ts
@@ -68,6 +68,13 @@ export const regionTimezoneMap: Record<Region, string> = {
  */
 export const latestAppConfig: AppConfig[] = [
   {
+    id: 6753102795,
+    name: {
+      mo: 'Zero One Progrise',
+    },
+    addType: 'auto',
+  },
+  {
     id: 372731237,
     name: {
       mo: 'Fusion Calculator',
